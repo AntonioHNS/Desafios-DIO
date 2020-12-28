47997b257aebdd0a6cd4e4ecb5716ec66a9f2efb
Antonio Soares <soaresantonio28@gmail.com>
2020-12-28 11:09:13 -0300

    Update divisao.cs

--- Introdução a programação/divisao.cs
+++ Introdução a programação/divisao.cs
@@ -5,14 +5,17 @@ class Desafio {
         int limit = Int32.Parse(Console.ReadLine());
         for (int i = 0; i < limit; i++) {
             string[] line = Console.ReadLine().Split(" ");
-            int X = int.Parse(line[0]);
-            int Y = int.Parse(line[1]);
+            double X = double.Parse(line[0]);
+            double Y = double.Parse(line[1]);
             if (Y == 0) {
                 Console.WriteLine("divisao impossivel");
             } else {
-                decimal divisao = decimal.Parse(X/Y);
-                Console.WriteLine(divisao.ToString("N1"));
-                
+                double divisao = X / Y;
+                if (divisao < 0 && (divisao.ToString("N1") == "0.0")){
+                   Console.WriteLine("-0.0");
+                } else {
+                  Console.WriteLine(divisao.ToString("N1"));
+                }
             }
         }
     }
